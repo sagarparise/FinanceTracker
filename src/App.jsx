230ae2eb5e9230d00bbdb9b19c0ab781d1e8f97f
sagar parise <sagarparise01@gmail.com>
2024-04-02 11:30:39 +0530

    privateRoute

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,22 @@ import Login from "./Components/Login/Login";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import Error from "./Components/Error";
 import {ToastContainer, toast} from 'react-toastify'
-import {notification} from 'antd'
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./Firebase";
 function App() {
-  const [api, contextHolder] = notification.useNotification();
+  const [user] = useAuthState(auth);
 
   return (
     <>
-    {contextHolder}
+   
     <ToastContainer />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard/>} />
+        {user && <Route path="/dashboard" element={<Dashboard/>} />}
           <Route path="/*" element={<Error />} />
         </Routes>
       </BrowserRouter>
